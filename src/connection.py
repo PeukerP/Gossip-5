5924b55a59a181062917fdf775529893dc7591c5
Petra Peuker <petra.peuker@tum.de>
2022-07-10 21:14:24 +0200

    AF according to host ip address. Assemble/disassemble message header. Use enum for message types

--- src/connection.py
+++ src/connection.py
@@ -1,30 +1,38 @@
 import asyncio
 import socket
-from struct import unpack
+from struct import unpack, pack
+from util import MessageType
 
 
 class Server():
 
-    def __init__(self, host, port, send_queue: asyncio.PriorityQueue, recv_queue: asyncio.PriorityQueue, send_lock: asyncio.Lock, recv_lock: asyncio.Lock):
+    def __init__(self, host, port, send_queue: asyncio.PriorityQueue, recv_queue: asyncio.PriorityQueue):
 
         self.host = host
         self.port = port
-        # Irgendwie müssen connctions verwaltet werden.
-        # Solange bis klar, wie genau, ist das ein set aus (reader, writer)
+        if host.find("."):
+            self.addresses_family = socket.AddressFamily.AF_INET
+        elif host.find(":"):
+            self.addresses_family = socket.AddressFamily.AF_INET6
+        else:
+            print("Address Family is not known")
+            exit(-1)
+
+        # Irgendwie müssen connctions verwaltet werden?
+        # Solange bis klar, wie genau, ist das eine map (adddr, port) -> (reder, writer)
         self.__connections = {}
         # Gossip -> out
         self.send_queue = send_queue
-        self.send_lock = send_lock
         # out -> Gossip
         self.recv_queue = recv_queue
-        self.recv_lock = recv_lock
 
     async def __establish_connection(self, host, port):
+        # TODO: POW? Authentication?
         reader, writer = await asyncio.open_connection(host=host, port=port)
         self.__connections.update({(host, port): (reader, writer)})
         return reader, writer
 
-    async def __send_msg(self, addr, port, message):
+    async def __send_msg(self, addr, port, message_type, message):
         writer = None
         if (addr, port) in self.__connections:
             writer = self.__connections[(addr, port)]
@@ -33,39 +41,58 @@ class Server():
 
         if writer.is_closing():
             return ""
-        #TODO: Can we use API messages for p2p layer??? Otherwise new subclass
-        writer.write(message)
+
+        msg = pack(">H", len(message))
+        msg += pack(">H", message_type)
+        msg += message
+
+        writer.write(msg)
         await writer.drain()
 
     async def __handle_sending(self):
-        while True:
-            async with self.send_lock:
-                addr, port, msg = await self.send_queue.get()
-            self.__send_msg(addr, port, message)
+        addr, port, msg_type, msg = (await self.send_queue.get())[1]
+        self.__send_msg(addr, port, msg_type, msg)
+        self.send_queue.task_done() # Noetig?
 
-
+    '''
+    Reads a message from an existing reader and returns a tuple (ret, msg).
+    ret is -1 if connection is closing/closed, 1 on an error and 0, if we have a message
+    msg is a triple of message size, message type and the message itself.
+    '''
     async def __read_msg(cls, reader, writer):
-       # TODO: error handling
         # Whether connection is open can only be tested with writer
         if writer.is_closing():
             return (-1, b"")
 
+        # Read message header
+        # Read size of package
         msg_size = await reader.read(2)
         if (msg_size == b''):
-            return (0, msg_size)        
+            return (1, b"")
         size = unpack(">H", msg_size)[0]
+        # Read message type
+        msg_type = await reader.read(2)
+        if (msg_type == b""):
+            return (1, b"")
+        m_type = unpack(">H", msg_type)[0]
         #print("Found message of length", size)
-        msg = msg_size + await reader.read(size-2)
-        if (msg == msg_size and size > 0):
-            return (0, msg_size)  
-        #print(msg)
+        # Read message
+        message = await reader.read(size-4)
+        if (message == b''):
+            return (1, b"")
 
-        # TODO: Prio according message type -> new class -> how to handle inhertitance???
-        return (1, msg)
+        msg = msg_size, msg_type, message
+        # print(msg)
 
-    async def __pass_to_recv_queue(self, msg):
-        async with self.recv_lock:
-            await self.recv_queue.put(msg)
+        return (0, msg)
+
+    async def __pass_to_recv_queue(self, msg_type, msg):
+        if msg_type == MessageType.GOSSIP_VALIDATION:
+            # validation has a higher prio
+            await self.recv_queue.put((0, msg))
+        else :
+            await self.recv_queue.put((1, msg))
+
 
     async def __handle_connection(self, reader: asyncio.StreamReader,  writer: asyncio.StreamWriter):
         addr, port = writer.get_extra_info('socket').getpeername()
@@ -79,20 +106,23 @@ class Server():
                 # Connection is lost
                 self.__connections.popitem((addr, port))
                 return
-            if msg[1] == b'':
-                continue
-            await self.__pass_to_recv_queue(msg)
+            if msg[0] == 1:
+                # No message received
+                return
+            else:
+                await self.__pass_to_recv_queue(msg[1][1], msg[1][2])
 
     def start(self):
         eloop = asyncio.get_event_loop()
         server = asyncio.start_server(self.__handle_connection,
                                       host=self.host,
                                       port=self.port,
-                                      family=socket.AddressFamily.AF_INET,
+                                      family=self.addresses_family,
                                       reuse_address=True,
                                       reuse_port=True)
         eloop.create_task(server)
         eloop.create_task(self.__handle_sending())
+        # Evtl das in init, da noch gossip tasks generiert werden müssen
         try:
             eloop.run_forever()
         except KeyboardInterrupt as e:
