73f25829187f7c918a2e98346a30770ee2e0f4d4
Petra Peuker <petra.peuker@tum.de>
2022-07-12 22:55:44 +0200

    Improved init.py. Added Message Types to enum. Incomplete bootstapping implementation attempt

--- src/connection.py
+++ src/connection.py
@@ -3,23 +3,20 @@ import socket
 from struct import unpack, pack
 from util import MessageType
 
+# TODO: Make ipv4 AND ipv6 sockets possible
+# Disclaimer: Bootstrapping is not implemented yet. And until now, technically we only support ipv4
+# Maybe give up the Idea of simply use asyncio.start_server?
+
 
 class Server():
 
-    def __init__(self, host, port, send_queue: asyncio.PriorityQueue, recv_queue: asyncio.PriorityQueue, event_loop):
+    def __init__(self, host, port, send_queue: asyncio.PriorityQueue, recv_queue: asyncio.PriorityQueue, event_loop, degree=-1, bootstrapper=None):
 
         self.host = host
         self.port = port
-        if host.find("."):
-            self.addresses_family = socket.AddressFamily.AF_INET
-        elif host.find(":"):
-            self.addresses_family = socket.AddressFamily.AF_INET6
-        else:
-            print("Address Family is not known")
-            exit(-1)
-
-        # Irgendwie müssen connctions verwaltet werden?
-        # Solange bis klar, wie genau, ist das eine map (adddr, port) -> (reder, writer)
+        self.bootstrapper = bootstrapper
+        self.degree = degree
+        self.__peer_list = []
         self.__connections = {}
         # Gossip -> out
         self.send_queue = send_queue
@@ -27,33 +24,67 @@ class Server():
         self.recv_queue = recv_queue
         self.eloop = event_loop
 
-    async def __establish_connection(self, host, port):
-        # TODO: POW? Authentication?
-        reader, writer = await asyncio.open_connection(host=host, port=port)
-        self.__connections.update({(host, port): (reader, writer)})
+    async def __establish_connection(self, address, port):
+        reader, writer = await asyncio.open_connection(address, port)
+        self.__connections.update({(address, port): (reader, writer)})
         return reader, writer
 
-    async def __send_msg(self, addr, port, message_type, message):
-        writer = None
-        if (addr, port) in self.__connections:
-            writer = self.__connections[(addr, port)]
-        else:
-            writer = self.__establish_connection(addr, port)[1]
+    async def __send_peer_response(self, message):
+        # Since only the server knows its neighbors, we have to assemble this package here
+        # In this case, message contains (limit, nonce)
 
-        if writer.is_closing():
-            return ""
+        # We now assume, that there are only ipv4 addresses. TODO: change this
+        # TODO: Limit number of peers!!!!!!!!1
+        return
 
-        msg = pack(">H", len(message))
-        msg += pack(">H", message_type)
-        msg += message
+        msg += pack(">HQQ", message_type, nonce, do_pow(nonce))
+        msg += pack(">L", 0)
+        msg += socket.inet_aton(p[0])
+        msg += pack(">H", p[1])
+        msg = pack(">H", 2 + len(msg)) + msg
 
-        writer.write(msg)
-        await writer.drain()
+    '''
+    Send message to all the known peers. If a peer has dropped out, we 
+    remove it from the known-peers-list
+    '''
+
+    async def __send_msg(self, message_size, message_type: MessageType, message):
+
+        peers_to_remove = []
+        msg = b''
+        i = 0
+
+        if message_type == MessageType.GOSSIP_PEER_RESPONSE:
+            self.__send_peer_response(message)
+            return
+
+        for peer in self.__peer_list:
+            writer = None
+            if peer in self.__connections:
+                writer = self.__connections[peer][1]
+            else:
+                reader, writer = self.__establish_connection(peer[0], peer[1])
+
+            if writer.is_closing():
+                # Remember to remove
+                peers_to_remove.append((peer, (reader, writer)))
+                continue
+
+            msg += pack(">H", message_size)
+            msg += pack(">H", message_type)
+            msg += message
+
+            writer.write(msg)
+            await writer.drain()
+        # Remove from peer-list
+        for peer in peers_to_remove:
+            self.__peer_list.remove(peer)
+            self.__connections.popitem(peer)
 
     async def __handle_sending(self):
-        addr, port, msg_type, msg = (await self.send_queue.get())[1]
-        self.__send_msg(addr, port, msg_type, msg)
-        self.send_queue.task_done() # Noetig?
+        msg_size, msg_type, msg = (await self.send_queue.get())
+        self.__send_msg(msg_size, msg_type, msg)
+        self.send_queue.task_done()  # Noetig?
 
     '''
     Reads a message from an existing reader and returns a tuple (ret, msg).
@@ -68,7 +99,7 @@ class Server():
         # Read message header
         # Read size of package
         msg_size = await reader.read(2)
-        if (msg_size == b''):
+        if (msg_size == b'' or len(msg_size) < 2):
             return (1, b"")
         size = unpack(">H", msg_size)[0]
         # Read message type
@@ -86,17 +117,22 @@ class Server():
 
         return (0, msg)
 
-    async def __pass_to_recv_queue(self, msg_type, msg):
-        if msg_type == MessageType.GOSSIP_VALIDATION:
+    async def __pass_to_recv_queue(self, msg: tuple):
+        if msg[1] == MessageType.GOSSIP_VALIDATION:
             # validation has a higher prio
             await self.recv_queue.put((0, msg))
-        else :
+        else:
             await self.recv_queue.put((1, msg))
-
 
     async def __handle_connection(self, reader: asyncio.StreamReader,  writer: asyncio.StreamWriter):
         addr, port = writer.get_extra_info('socket').getpeername()
         # Register connection
+        if len(self.__peer_list) == self.degree:
+            # remove first connection
+            self.__peer_list.pop(0)
+
+        # TODO: Send challenge + check response before adding to peers-list
+        self.__peer_list.append(((addr, port)))
         self.__connections.update({(addr, port): (reader, writer)})
 
         # Read and hand over to gossip
@@ -104,6 +140,7 @@ class Server():
             msg = await self.__read_msg(reader, writer)
             if (msg[0] == -1):
                 # Connection is lost
+                self.__peer_list.remove((addr, port))
                 self.__connections.popitem((addr, port))
                 return
             if msg[0] == 1:
@@ -111,15 +148,29 @@ class Server():
                 return
             else:
                 print(msg)
-                await self.__pass_to_recv_queue(msg[1][1], msg[1][2])
+                await self.__pass_to_recv_queue(msg[1])
+
+    def init_bootstrap(self):
+        # Build GOSSIP PEER REQUEST
+        msg = b''
+        msg += pack(">HL", MessageType.GOSSIP_PEER_REQUEST, self.degree)
+        # TODO: sinnvoller Wert für die nonce
+        msg += pack(">Q", 0)
+
+        msg = pack(">H", 2 + len(msg)) + msg
+
+        # Put message into send_queue
+        self.send_queue.put((len(msg), MessageType.GOSSIP_PEER_REQUEST, msg))
 
     def start(self):
+        # Todo: Put a peer request into send queue
+        if self.bootstrapper is not None:
+            self.init_bootstrap()
         server = asyncio.start_server(self.__handle_connection,
                                       host=self.host,
                                       port=self.port,
-                                      family=self.addresses_family,
+                                      family=socket.AF_UNSPEC,
                                       reuse_address=True,
                                       reuse_port=True)
         self.eloop.create_task(server)
         self.eloop.create_task(self.__handle_sending())
-        
