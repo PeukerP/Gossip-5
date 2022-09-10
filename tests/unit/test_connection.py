b330fcf39c94bf9cbbaa3a274afcb7735f435855
Petra Peuker <petra.peuker@tum.de>
2022-09-10 12:19:53 +0200

    Fix test_connection

--- tests/unit/test_connection.py
+++ tests/unit/test_connection.py
@@ -1,10 +1,9 @@
-import logging
 import asyncio
 from connection import Connections
 from unittest import IsolatedAsyncioTestCase, main
 from unittest.mock import Mock, patch
 
-logger = logging.getLogger("Test")
+logger = Mock()
 
 
 class TestConnections(IsolatedAsyncioTestCase):
@@ -25,49 +24,49 @@ class TestConnections(IsolatedAsyncioTestCase):
             c = Connections(logger)
 
             await c.update_connection("a", None, None)
-            assert c.get_all_connections() == {"a": (None,None)}
+            assert c.get_all_connections() == {"a": (None, None)}
             # Test upgrade
             await c.update_connection("a", 1, 1)
-            assert c.get_all_connections() == {"a": (1,1)}
+            assert c.get_all_connections() == {"a": (1, 1)}
             # Test no overwrite
             await c.update_connection("a", 1, 2)
-            assert c.get_all_connections() == {"a": (1,1)}
+            assert c.get_all_connections() == {"a": (1, 1)}
 
             await c.update_connection("b", 2, 2)
-            assert c.get_all_connections() == {"a": (1,1), "b": (2,2)}
+            assert c.get_all_connections() == {"a": (1, 1), "b": (2, 2)}
 
     async def test_update_connection_overwrite(self):
         with patch.object(Connections, "is_alive", return_value=False):
             c = Connections(logger)
 
             await c.update_connection("a", None, None)
-            assert c.get_all_connections() == {"a": (None,None)}
+            assert c.get_all_connections() == {"a": (None, None)}
 
             await c.update_connection("a", 1, 1)
-            assert c.get_all_connections() == {"a": (1,1)}
+            assert c.get_all_connections() == {"a": (1, 1)}
             # Test overwrite
             await c.update_connection("a", 1, 2)
-            assert c.get_all_connections() == {"a": (1,2)}
+            assert c.get_all_connections() == {"a": (1, 2)}
 
             await c.update_connection("b", 2, 2)
-            assert c.get_all_connections() == {"a": (1,2), "b": (2,2)}
+            assert c.get_all_connections() == {"a": (1, 2), "b": (2, 2)}
 
     async def test_update_connection_limit_not_alive(self):
         with patch.object(Connections, "is_alive", return_value=False):
             # Stream Mock
             mock_s = Mock()
-            mock_s.is_closing.return_value=False
+            mock_s.is_closing.return_value = False
 
             c = Connections(logger)
             c.set_limit(1)
             await c.update_connection("a", None, None)
-            assert c.get_all_connections() == {"a": (None,None)}
+            assert c.get_all_connections() == {"a": (None, None)}
 
             await c.update_connection("a", 1, mock_s)
-            assert c.get_all_connections() == {"a": (1,mock_s)}
+            assert c.get_all_connections() == {"a": (1, mock_s)}
 
             await c.update_connection("b", 1, 1)
-            assert c.get_all_connections() == {"b": (1,1)}
+            assert c.get_all_connections() == {"b": (1, 1)}
 
     async def test_update_connection_limit_alive(self):
         with patch.object(Connections, "is_alive", return_value=True):
@@ -75,20 +74,20 @@ class TestConnections(IsolatedAsyncioTestCase):
             f = asyncio.Future()
             f.set_result(None)
             mock_s = Mock()
-            mock_s.is_closing.return_value=False
-            mock_s.close.return_value=None
-            mock_s.wait_closed.return_value=f
+            mock_s.is_closing.return_value = False
+            mock_s.close.return_value = None
+            mock_s.wait_closed.return_value = f
 
             c = Connections(logger)
             c.set_limit(1)
             await c.update_connection("a", None, None)
-            assert c.get_all_connections() == {"a": (None,None)}
+            assert c.get_all_connections() == {"a": (None, None)}
 
             await c.update_connection("a", 1, mock_s)
-            assert c.get_all_connections() == {"a": (1,mock_s)}
+            assert c.get_all_connections() == {"a": (1, mock_s)}
 
             await c.update_connection("b", 1, 1)
-            assert c.get_all_connections() == {"b": (1,1)}
+            assert c.get_all_connections() == {"b": (1, 1)}
 
     async def test_get_random_peers(self):
         c = Connections(logger)
@@ -106,7 +105,7 @@ class TestConnections(IsolatedAsyncioTestCase):
     async def test_remove_connection_None(self):
         c = Connections(logger)
         mock = Mock()
-        mock.is_closing.return_value=False
+        mock.is_closing.return_value = False
         await c.update_connection("a", None, None)
         await c.remove_connection("a")
         assert c.get_all_connections() == {}
@@ -114,16 +113,15 @@ class TestConnections(IsolatedAsyncioTestCase):
     async def test_remove_connection(self):
         with patch.object(Connections, "is_alive", return_value=True):
             c = Connections(logger)
-            mock = Mock()
-            mock.is_closing.return_value=False
+            mock = generate_stream_mock(False)
+
             await c.update_connection("a", 1, mock)
             await c.remove_connection("a")
             assert c.get_all_connections() == {}
 
     async def test_remove_connection_not_in(self):
         c = Connections(logger)
-        mock = Mock()
-        mock.is_closing.return_value=False
+        mock = generate_stream_mock(False)
         await c.update_connection("a", 0, mock)
         await c.remove_connection("b")
         assert c.get_all_connections() == {"a": (0, mock)}
@@ -131,77 +129,43 @@ class TestConnections(IsolatedAsyncioTestCase):
     async def test_is_alive_1(self):
         # Peer exists in list and is alive
         # Stream Mock
-        f = asyncio.Future()
-        f.set_result(None)
-
-        mock_s = Mock()
-        mock_s.is_closing.return_value=False
-        mock_s.close.return_value=None
-        mock_s.wait_closed.return_value=f
-        mock_s.write.return_value=None
-        mock_s.drain.return_value=f
+        mock_s = generate_stream_mock(False)
 
         with patch.object(Connections, "establish_connection", return_value=(Mock(), mock_s)):
             c = Connections(logger)
             await c.update_connection("a", 0, mock_s)
-            assert await c.is_alive("a") == True
+            assert await c.is_alive("a")
             mock_s.close.assert_not_called()
             mock_s.wait_closed.assert_not_called()
-            
+
     async def test_is_alive_2(self):
         # Peer does not exist in list, but is alive
         # Stream Mock
-        f = asyncio.Future()
-        f.set_result(None)
-
-        mock_s = Mock()
-        mock_s.is_closing.return_value=False
-        mock_s.close.return_value=None
-        mock_s.wait_closed.return_value=f
-        mock_s.write.return_value=None
-        mock_s.drain.return_value=f
+        mock_s = generate_stream_mock(False)
 
         with patch.object(Connections, "establish_connection", return_value=(Mock(), mock_s)):
             c = Connections(logger)
-            assert await c.is_alive("a") == True
-            mock_s.close.assert_called_once()
-            mock_s.wait_closed.assert_called_once()
+            assert not await c.is_alive("a")
 
     async def test_is_alive_3(self):
         # Peer exist in list and is not alive
         # Stream Mock
-        f = asyncio.Future()
-        f.set_result(None)
-
-        mock_s = Mock()
-        mock_s.is_closing.return_value=False
-        mock_s.close.return_value=None
-        mock_s.wait_closed.return_value=f
-        mock_s.write.side_effect=Exception
-        mock_s.drain.return_value=f
+        mock_s = generate_stream_mock(False)
+        mock_s.write.side_effect = Exception
 
         with patch.object(Connections, "establish_connection", return_value=(None, None)):
             c = Connections(logger)
             await c.update_connection("a", 0, mock_s)
-            assert await c.is_alive("a") == False
-
+            assert not await c.is_alive("a")
 
     async def test_is_alive_4(self):
         # Peer exist not list and is not alive
-        # Stream Mock
-        f = asyncio.Future()
-        f.set_result(None)
-
-        mock_s = Mock()
-        mock_s.is_closing.return_value=False
-        mock_s.close.return_value=None
-        mock_s.wait_closed.return_value=f
-        mock_s.write.side_effect=Exception
-        mock_s.drain.return_value=f
+        mock_s = generate_stream_mock(False)
+        mock_s.write.side_effect = Exception
 
         with patch.object(Connections, "establish_connection", return_value=(None, mock_s)):
             c = Connections(logger)
-            assert await c.is_alive("a") == False
+            assert not await c.is_alive("a")
             mock_s.close.assert_not_called()
             mock_s.wait_closed.assert_not_called()
 
@@ -209,49 +173,55 @@ class TestConnections(IsolatedAsyncioTestCase):
         c = Connections(logger)
         assert await c.establish_connection("a") == (None, None)
 
-
     async def test_establish_connection_2(self):
         # Connection works
         # Stream Mock
-        
+
         mock = Mock()
-        mock.get_extra_info.return_value="Test"
+        mock.get_extra_info.return_value = "Test"
 
-        peer  = Mock()
-        peer.ip="Test"
-        peer.port="123"
+        peer = Mock()
+        peer.ip = "Test"
+        peer.port = "123"
 
-        with patch("asyncio.open_connection", return_value=(mock,mock)):
+        with patch("asyncio.open_connection", return_value=(mock, mock)):
             c = Connections(logger)
             await c.update_connection(peer, None, None)
             assert await c.establish_connection(peer) == (mock, mock)
-            assert c.get_all_connections() == {peer: (mock,mock)}
+            assert c.get_all_connections() == {peer: (mock, mock)}
 
     async def test_establish_connection_3(self):
         # Connection fails
         # Stream Mock
-        
-        mock = Mock()
-        mock.get_extra_info.return_value="Test"
 
-        peer  = Mock()
-        peer.ip="Test"
-        peer.port="123"
+        mock = Mock()
+        mock.get_extra_info.return_value = "Test"
+
+        peer = Mock()
+        peer.ip = "Test"
+        peer.port = "123"
 
         with patch("asyncio.open_connection", side_effect=Exception):
             c = Connections(logger)
             await c.update_connection(peer, None, None)
             assert await c.establish_connection(peer) == (None, None)
             assert c.get_all_connections() == {}
-    
+
+
+def generate_stream_mock(is_closing):
+    f = asyncio.Future()
+    f.set_result(None)
+
+    mock_s = Mock()
+    mock_s.is_closing.return_value = is_closing
+    mock_s.close.return_value = None
+    mock_s.wait_closed.return_value = f
+    mock_s.drain.return_value = f
+
+    return mock_s
+
+
 if __name__ == "__main__":
+    logger.exception.return_value = None
+    logger.warning.return_value = None
     main()
-    
-        
-
-    
-
-    
-
-        
-
