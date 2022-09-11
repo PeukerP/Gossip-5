b9d97b3961e290c5c1c606ecf5415ed8cc1cb8f4
Thomas Lagemann <lagemann@in.tum.de>
2022-09-11 14:29:00 +0200

    Advancements for Testing and debugging

--- src/gossip_logic.py
+++ src/gossip_logic.py
@@ -39,7 +39,7 @@ class GossipHandler:
         self.suppress_circular_messages_time = suppress_circular_messages_time
         self.validation_wait_time = validation_wait_time
 
-    async def __handle_external(self, message_type: MessageType, sender: Peer, message: bytes):
+    async def _handle_external(self, message_type: MessageType, sender: Peer, message: bytes):
         """
         Handler to process messages from p2p_recv_queue.
 
@@ -52,11 +52,11 @@ class GossipHandler:
             data_type: bytes = message[8:10]
             data: bytes = message[10:]
             if msg_id not in self.spread:
-                await self.__send_notification(ttl, msg_id, data_type, data)
+                await self._send_notification(ttl, msg_id, data_type, data)
         else:  # MessageType unknown or illegal
             self.logger.warning("Unknown or illegal MessageType received from other peer(%s): %s", sender, message_type)
 
-    async def __handle_internal(self, message_type: MessageType, sender: Peer, message: bytes):
+    async def _handle_internal(self, message_type: MessageType, sender: Peer, message: bytes):
         """
         Handler to process messages from api_recv_queue.
 
@@ -65,11 +65,11 @@ class GossipHandler:
         :param message: The received qpi message as bytes.
         """
         if message_type == MessageType.GOSSIP_NOTIFY:
-            self.__notify(sender, message)
+            self._notify(sender, message)
         elif message_type == MessageType.GOSSIP_ANNOUNCE:
-            await self.__forward_announce(message)
+            await self._forward_announce(message)
         elif message_type == MessageType.GOSSIP_VALIDATION:
-            await self.__validate(message)
+            await self._validate(message)
         else:
             self.logger.warning("Unknown or illegal MessageType received from module(%s): %s", sender, message_type)
 
@@ -79,7 +79,7 @@ class GossipHandler:
         """
         while True:
             (size, msg_type, body, message), sender = await self.p2p_recv_queue.get()
-            await self.__handle_external(msg_type, sender, message)
+            await self._handle_external(msg_type, sender, message)
 
     async def __listen_recv_api(self):
         """
@@ -87,9 +87,9 @@ class GossipHandler:
         """
         while True:
             (size, msg_type, body, message), sender = await self.api_recv_queue.get()
-            await self.__handle_internal(msg_type, sender, message)
+            await self._handle_internal(msg_type, sender, message)
 
-    async def __forward_announce(self, message: bytes):
+    async def _forward_announce(self, message: bytes):
         """
         Handles a received GOSSIP_ANNOUNCE message. Generates a new messageID and calls send_notification()
 
@@ -99,9 +99,9 @@ class GossipHandler:
         data_type = message[6:8]
         data = message[8:]
         msg_id: bytes = random.randbytes(2)
-        await self.__send_notification(ttl + 1, msg_id, data_type, data)
+        await self._send_notification(ttl + 1, msg_id, data_type, data)
 
-    def __notify(self, sender: Peer, message: bytes):
+    def _notify(self, sender: Peer, message: bytes):
         """
         Consumes a received GOSSIP_NOTIFY message and saves the Module to be notified
 
@@ -111,7 +111,9 @@ class GossipHandler:
         data_type = message[6:8]
         self.notify.append(Module(sender, data_type))
 
-    async def __send_notification(self, ttl: int, msg_id: bytes, data_type: bytes, data: bytes):
+        self.logger.debug("Module %s asked to get notified for data type %s", sender, data_type)
+
+    async def _send_notification(self, ttl: int, msg_id: bytes, data_type: bytes, data: bytes):
         """
         Sends GOSSIP_NOTIFICATION to Modules which are to be notified for given data_type. Stores message in temporary
         dictionary to spread the message to other peers when it can be validated.
@@ -122,7 +124,7 @@ class GossipHandler:
         :param data_type: The data_type of the message to be spread as bytes.
         :param data: The data of the message to be spread as bytes.
         """
-        self.__remove_old_waiting()
+        self._remove_old_waiting()
         validate: bool = False
         message: bytes = MessageType.GOSSIP_NOTIFICATION.to_bytes(2, 'big') + msg_id + data_type + data
         message = (len(message) + 2).to_bytes(2, 'big') + message
@@ -136,8 +138,9 @@ class GossipHandler:
                                   (ttl - 1).to_bytes(1, 'big') + b'0' + msg_id + data_type + data
             peer_message = (len(peer_message) + 2).to_bytes(2, 'big') + peer_message
             self.waiting_for_validation.update({msg_id: (peer_message, time.time())})
+            self.logger.debug("Message is waiting for validation: msgID %s ", msg_id)
 
-    def __remove_old_waiting(self):
+    def _remove_old_waiting(self):
         """
         Removes expired messages that are waiting for validation from the dictionary after defined time in
         validation_wait_time.
@@ -145,8 +148,9 @@ class GossipHandler:
         for msg_id in self.waiting_for_validation:  # Remove messages when waiting to long
             if time.time() - self.waiting_for_validation[msg_id] > self.validation_wait_time:
                 del self.waiting_for_validation[msg_id]
+                self.logger.debug("Removed old message from spread list: msgID %s", msg_id)
 
-    async def __validate(self, message: bytes):
+    async def _validate(self, message: bytes):
         """
         Consumes a GOSSIP_VALIDATION message and checks for the validation bit. Releases a GOSSIP_PEER_NOTIFICATION
         message if the validation bit is set and a message with given messageID is waiting for validation.
@@ -160,10 +164,14 @@ class GossipHandler:
                 peer_notification_message: bytes = self.waiting_for_validation[msg_id][0]
                 await self.p2p_send_queue.put((peer_notification_message, None))
                 self.spread.update({msg_id: time.time()})
-                self.__remove_old_spread()
+                self.logger.debug("Message was spread: msgID %s ", msg_id)
+                self.logger.debug("Sent validated message from waiting list: msgID %s", msg_id)
+                self._remove_old_spread()
+            else:
+                self.logger.warning("Message validation failed, message will be removed and not send: msgID %s", msg_id)
             del self.waiting_for_validation[msg_id]
 
-    def __remove_old_spread(self):
+    def _remove_old_spread(self):
         """
         Removes expired messageIDs from the dictionary with already spread messages after defined time in
         suppress_circular_messages_time.
@@ -171,6 +179,7 @@ class GossipHandler:
         for msg_id in self.spread:
             if time.time() - self.spread[msg_id] > self.suppress_circular_messages_time:
                 del self.spread[msg_id]
+                self.logger.debug("Removed old msgID from spread list: %s", msg_id)
 
     def start(self):
         """
