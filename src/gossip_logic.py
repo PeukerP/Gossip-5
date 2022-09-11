c57a5dc5afae695cc69c702835d2852ab74f8612
Thomas Lagemann <t.lagemann@tum.de>
2022-09-11 22:59:19 +0200

    Tests for gossip_logic.py

--- src/gossip_logic.py
+++ src/gossip_logic.py
@@ -3,7 +3,7 @@ import logging
 import random
 import time
 
-from util import MessageType, Module, Peer
+from src.util import MessageType, Module, Peer
 
 
 class GossipHandler:
@@ -39,7 +39,7 @@ class GossipHandler:
         self.suppress_circular_messages_time = suppress_circular_messages_time
         self.validation_wait_time = validation_wait_time
 
-    async def _handle_external(self, message_type: MessageType, sender: Peer, message: bytes):
+    async def __handle_external(self, message_type: MessageType, sender: Peer, message: bytes):
         """
         Handler to process messages from p2p_recv_queue.
 
@@ -53,11 +53,11 @@ class GossipHandler:
             data: bytes = message[10:]  # Get message data
             if msg_id not in self.spread and msg_id not in self.waiting_for_validation:
                 # If messageID is not already known
-                await self._send_notification(ttl, msg_id, data_type, data)  # Send message
+                await self.__send_notification(ttl, msg_id, data_type, data)  # Send message
         else:  # MessageType is unknown or illegal
             self.logger.warning("Unknown or illegal MessageType received from other peer(%s): %i", sender, message_type)
 
-    async def _handle_internal(self, message_type: MessageType, sender: Peer, message: bytes):
+    async def __handle_internal(self, message_type: MessageType, sender: Peer, message: bytes):
         """
         Handler to process messages from api_recv_queue.
 
@@ -66,11 +66,11 @@ class GossipHandler:
         :param message: The received qpi message as bytes.
         """
         if message_type == MessageType.GOSSIP_NOTIFY:  # Handle GOSSIP_NOTIFY
-            self._notify(sender, message)
+            self.__notify(sender, message)
         elif message_type == MessageType.GOSSIP_ANNOUNCE:  # Handle GOSSIP_ANNOUNCE
-            await self._forward_announce(message)
+            await self.__forward_announce(message)
         elif message_type == MessageType.GOSSIP_VALIDATION:  # Handle GOSSIP_VALIDATION
-            await self._validate(message)
+            await self.__validate(message)
         else:  # MessageType is unknown or illegal
             self.logger.warning("Unknown or illegal MessageType received from module(%s): %i", sender, message_type)
 
@@ -83,7 +83,7 @@ class GossipHandler:
                 (size, msg_type, body, message), sender = await self.p2p_recv_queue.get()
                 # Get oldest unhandled message
                 self.logger.debug("Gossip got P2P message from %s", sender)
-                await self._handle_external(msg_type, sender, message)  # Handle p2p message
+                await self.__handle_external(msg_type, sender, message)  # Handle p2p message
         except RuntimeError as e:
             if e.args[0] == "Event loop is closed":
                 self.logger.debug("P2P queue was closed.")
@@ -99,14 +99,14 @@ class GossipHandler:
                 (size, msg_type, body, message), sender = await self.api_recv_queue.get()
                 # Get oldest unhandled message
                 self.logger.debug("Gossip got api message from %s", sender)
-                await self._handle_internal(msg_type, sender, message)  # Handle api message
+                await self.__handle_internal(msg_type, sender, message)  # Handle api message
         except RuntimeError as e:
             if e.args[0] == "Event loop is closed":
                 self.logger.debug("Stopped: API queue was closed.")
             else:
                 raise e
 
-    async def _forward_announce(self, message: bytes):
+    async def __forward_announce(self, message: bytes):
         """
         Handles a received GOSSIP_ANNOUNCE message. Generates a new messageID and calls send_notification()
 
@@ -116,9 +116,9 @@ class GossipHandler:
         data_type = message[6:8]  # Get message data type
         data = message[8:]  # Get message data
         msg_id: bytes = random.randbytes(2)  # Generate new messageID
-        await self._send_notification(ttl + 1, msg_id, data_type, data)  # Forward message
+        await self.__send_notification(ttl, msg_id, data_type, data)  # Forward message
 
-    def _notify(self, sender: Peer, message: bytes):
+    def __notify(self, sender: Peer, message: bytes):
         """
         Consumes a received GOSSIP_NOTIFY message and saves the Module to be notified
 
@@ -129,7 +129,7 @@ class GossipHandler:
         self.notify.append(Module(sender, data_type))  # Add module to notifiers list
         self.logger.debug("Module %s asked to get notified for data type %s", sender, data_type)
 
-    async def _send_notification(self, ttl: int, msg_id: bytes, data_type: bytes, data: bytes):
+    async def __send_notification(self, ttl: int, msg_id: bytes, data_type: bytes, data: bytes):
         """
         Sends GOSSIP_NOTIFICATION to Modules which are to be notified for given data_type. Stores message in temporary
         dictionary to spread the message to other peers when it can be validated.
@@ -140,7 +140,7 @@ class GossipHandler:
         :param data_type: The data_type of the message to be spread as bytes.
         :param data: The data of the message to be spread as bytes.
         """
-        self._remove_old_waiting()  # Clean waiting dictionary
+        self.__remove_old_waiting()  # Clean waiting dictionary
         validate: bool = False
         message: bytes = MessageType.GOSSIP_NOTIFICATION.to_bytes(2, 'big') + msg_id + data_type + data
         message = (len(message) + 2).to_bytes(2, 'big') + message  # Build message for GOSSIP_NOTIFICATION
@@ -151,25 +151,28 @@ class GossipHandler:
                     validate = True  # Save state
         if validate and ttl > 0:  # When it has been sent and ttl allows, wait for validation to send to others
             peer_message: bytes = MessageType.GOSSIP_PEER_NOTIFICATION.to_bytes(2, 'big') + \
-                                  (ttl - 1).to_bytes(1, 'big') + b'0' + msg_id + data_type + data
+                                  (ttl - 1).to_bytes(1, 'big') + (0).to_bytes(1, 'big') + msg_id + data_type + data
             peer_message = (len(peer_message) + 2).to_bytes(2, 'big') + peer_message
             # Build message for GOSSIP_PEER_NOTIFICATION
             self.waiting_for_validation.update({msg_id: (peer_message, time.time())})
             # Add message to waiting dictionary
             self.logger.debug("Message is waiting for validation: msgID %s ", msg_id)
 
-    def _remove_old_waiting(self):
+    def __remove_old_waiting(self):
         """
         Removes expired messages that are waiting for validation from the dictionary after defined time in
         validation_wait_time.
         """
+        marked_for_deletion = []
         for msg_id in self.waiting_for_validation:  # Remove messages when waiting to long
-            if time.time() - self.waiting_for_validation[msg_id] > self.validation_wait_time:
+            if time.time() - self.waiting_for_validation[msg_id][1] > self.validation_wait_time:
                 # Check if limit is exceeded
-                del self.waiting_for_validation[msg_id]  # Remove expired message from waiting dictionary
+                marked_for_deletion.append(msg_id)  # Mark expired message to delete
                 self.logger.debug("Removed old message from spread dictionary: msgID %s", msg_id.decode)
+        for msg_id in marked_for_deletion:  # Delete old entries
+            del self.waiting_for_validation[msg_id]
 
-    async def _validate(self, message: bytes):
+    async def __validate(self, message: bytes):
         """
         Consumes a GOSSIP_VALIDATION message and checks for the validation bit. Releases a GOSSIP_PEER_NOTIFICATION
         message if the validation bit is set and a message with given messageID is waiting for validation.
@@ -185,22 +188,25 @@ class GossipHandler:
                 self.spread.update({msg_id: time.time()})  # Save message as spread
                 self.logger.debug("Message was spread: msgID %s ", msg_id)
                 self.logger.debug("Sent validated message from waiting dictionary: msgID %s", msg_id)
-                self._remove_old_spread()  # Clean spread dictionary
+                self.__remove_old_spread()  # Clean spread dictionary
             else:  # Message is not validate-able
                 self.logger.warning("Message validation failed, message will be removed and not send: msgID %s",
                                     msg_id)
             del self.waiting_for_validation[msg_id]
             # Remove messages that have been handled or that failed to be validated
 
-    def _remove_old_spread(self):
+    def __remove_old_spread(self):
         """
         Removes expired messageIDs from the dictionary with already spread messages after defined time in
         suppress_circular_messages_time.
         """
+        marked_for_deletion = []
         for msg_id in self.spread:  # Check all spread messageIDs
             if time.time() - self.spread[msg_id] > self.suppress_circular_messages_time:  # If msgID is expired
-                del self.spread[msg_id]  # Remove msgID from spread dictionary
+                marked_for_deletion.append(msg_id)  # Mark msgID to be removed
                 self.logger.debug("Removed old msgID from spread dictionary: %s", msg_id)
+        for msg_id in marked_for_deletion:  # Delete old entries
+            del self.spread[msg_id]
 
     def start(self):
         """
--- tests/unit/test_gossip_logic.py
+++ tests/unit/test_gossip_logic.py
@@ -1,36 +1,232 @@
 import asyncio
-from unittest import TestCase
+import random
+import time
+from unittest import IsolatedAsyncioTestCase, main
+from unittest.mock import MagicMock, patch
+from src.gossip_logic import GossipHandler
+from src.util import Module, MessageType, Peer
+from parameterized import parameterized
 
-api_recv_queue = asyncio.Queue()
-p2p_recv_queue = asyncio.Queue()
-api_send_queue = asyncio.Queue()
-p2p_send_queue = asyncio.Queue()
 
+class TestGossipHandler(IsolatedAsyncioTestCase):
 
-class TestGossipHandler(TestCase):
-    def test__handle_external(self):
-        self.fail()
+    @staticmethod
+    def new_basic_handler():
+        return GossipHandler(asyncio.Queue(), asyncio.Queue(), asyncio.Queue(), asyncio.Queue(), None, 600, 60)
 
-    def test__handle_internal(self):
-        self.fail()
+    @staticmethod
+    def get_data():
+        return random.randbytes(10)
 
-    def test__forward_announce(self):
-        self.fail()
+    @staticmethod
+    def get_data_type_or_msg_id():
+        return random.randbytes(2)
+
+    @staticmethod
+    def get_valid_ttl():
+        return random.randint(1, 3)
+
+    async def test__handle_external(self):
+        handler = self.new_basic_handler()
+        ttl, msg_id, data_type, data, n, peer_notification = self.__get_send_notification_basics(True, False, True)
+        with patch.object(GossipHandler, '_GossipHandler__send_notification') as mock:
+            await handler._GossipHandler__handle_external(MessageType.GOSSIP_PEER_NOTIFICATION, None, peer_notification)
+        mock.assert_called_with(ttl - 1, msg_id, data_type, data)
+
+        handler.spread.update({msg_id: time.time()})
+        with patch.object(GossipHandler, '_GossipHandler__send_notification') as mock:
+            await handler._GossipHandler__handle_external(MessageType.GOSSIP_PEER_NOTIFICATION, None, peer_notification)
+        mock.assert_not_called()
+
+    async def test__handle_internal(self):
+        handler = self.new_basic_handler()
+        sender = None
+        message = b''
+        with patch.object(GossipHandler, '_GossipHandler__notify') as mock:
+            await handler._GossipHandler__handle_internal(MessageType.GOSSIP_NOTIFY, sender, message)
+        mock.assert_called()
+
+        with patch.object(GossipHandler, '_GossipHandler__forward_announce') as mock:
+            await handler._GossipHandler__handle_internal(MessageType.GOSSIP_ANNOUNCE, sender, message)
+        mock.assert_called()
+
+        with patch.object(GossipHandler, '_GossipHandler__validate') as mock:
+            await handler._GossipHandler__handle_internal(MessageType.GOSSIP_VALIDATION, sender, message)
+        mock.assert_called()
+
+    @parameterized.expand([(0,), (1,), (2,), (20,), (50,)])
+    async def test__forward_announce(self, ttl: int):
+        handler = self.new_basic_handler()
+        data_type = self.get_data_type_or_msg_id()
+        data = self.get_data()
+        message = (18).to_bytes(2, 'big') + (500).to_bytes(2, 'big') + ttl.to_bytes(1, 'big') + b'0' + data_type + data
+        handler.notify.append(Module(None, data_type))
+        await handler._GossipHandler__forward_announce(message)
+        self.assertEqual(1, handler.api_send_queue.qsize())
+        if ttl > 0:
+            self.assertEqual(1, len(handler.waiting_for_validation))
+        else:
+            self.assertEqual(0, len(handler.waiting_for_validation))
 
     def test__notify(self):
-        self.fail()
+        handler = self.new_basic_handler()
+        data_type = self.get_data_type_or_msg_id()
+        message = (8).to_bytes(2, 'big') + (501).to_bytes(2, 'big') + b'00' + data_type
+        sender = Peer("1.1.1.1", 1111)
+        handler._GossipHandler__notify(sender, message)
+        temp = handler.notify.pop()
+        self.assertEqual(sender.port, temp.peer.port)
+        self.assertEqual(sender.ip, temp.peer.ip)
+        self.assertEqual(data_type, temp.type_of_data)
 
-    def test__send_notification(self):
-        self.fail()
+    @staticmethod
+    def __get_send_notification_basics(valid_ttl: bool, get_message: bool, get_peer: bool):
+        if valid_ttl:
+            ttl = TestGossipHandler.get_valid_ttl()
+        else:
+            ttl = 0
+        msg_id = TestGossipHandler.get_data_type_or_msg_id()
+        data_type = TestGossipHandler.get_data_type_or_msg_id()
+        data = TestGossipHandler.get_data()
+        notification_message = b''
+        peer_notification_message = b''
+        if get_message:
+            notification_message = (18).to_bytes(2, 'big') + (502).to_bytes(2, 'big') + msg_id + data_type + data
+        if get_peer and valid_ttl:
+            peer_notification_message = (20).to_bytes(2, 'big') + (511).to_bytes(2, 'big') + (ttl - 1).to_bytes(
+                1, 'big') + (0).to_bytes(1, 'big') + msg_id + data_type + data
+        elif get_peer:
+            print("get_peer can only be used with valid_ttl.")
+            raise RuntimeError("get_peer can only be used with valid_ttl.")
+        return ttl, msg_id, data_type, data, notification_message, peer_notification_message
 
-    def test__remove_old_waiting(self):
-        self.fail()
+    async def test__send_notification_no_notifiers(self):
+        handler = self.new_basic_handler()
+        ttl, msg_id, data_type, data, notification, peer_notification = self.__get_send_notification_basics(True, False,
+                                                                                                            False)
 
-    def test__validate(self):
-        self.fail()
+        await handler._GossipHandler__send_notification(ttl, msg_id, data_type, data)
 
-    def test__remove_old_spread(self):
-        self.fail()
+        self.assertTrue(handler.api_send_queue.empty())
+        self.assertTrue(handler.p2p_send_queue.empty())
+        self.assertEqual(0, len(handler.waiting_for_validation))
 
-    def test_start(self):
-        self.fail()
+    @parameterized.expand([(True,), (False,)])
+    async def test__send_notification_single_notifier(self, valid_ttl: bool):
+        handler = self.new_basic_handler()
+        ttl, msg_id, data_type, data, notification, peer_notification = self.__get_send_notification_basics(valid_ttl,
+                                                                                                            True,
+                                                                                                            valid_ttl)
+        handler.notify.append(Module(None, data_type))
+        await handler._GossipHandler__send_notification(ttl, msg_id, data_type, data)
+        if ttl > 0:
+            self.assertEqual(1, len(handler.waiting_for_validation))  # TTL > 0 -> spread message
+            self.assertEqual(peer_notification, handler.waiting_for_validation.get(msg_id)[0])
+        else:
+            self.assertEqual(0, len(handler.waiting_for_validation))
+        self.assertEqual(1, handler.api_send_queue.qsize())
+        self.assertTrue(handler.p2p_send_queue.empty())
+        temp = await handler.api_send_queue.get()
+        self.assertEqual(notification, temp[0])
+
+    async def test__send_notification_single_false_notifier(self):
+        handler = self.new_basic_handler()
+        ttl, msg_id, data_type, data, notification, peer_notification = self.__get_send_notification_basics(True, False,
+                                                                                                            False)
+        false_data_type = self.get_data_type_or_msg_id()
+        while false_data_type == data_type:
+            false_data_type = self.get_data_type_or_msg_id()
+        handler.notify.append(Module(None, false_data_type))
+        await handler._GossipHandler__send_notification(ttl, msg_id, data_type, data)
+        self.assertEqual(0, len(handler.waiting_for_validation))
+        self.assertEqual(0, handler.api_send_queue.qsize())
+        self.assertTrue(handler.p2p_send_queue.empty())
+
+    @parameterized.expand([(True,), (False,)])
+    async def test__send_notification_multiple_notifiers(self, valid_ttl: bool):
+        handler = self.new_basic_handler()
+        ttl, msg_id, data_type, data, notification, peer_notification = self.__get_send_notification_basics(valid_ttl,
+                                                                                                            True,
+                                                                                                            valid_ttl)
+        false_data_type = self.get_data_type_or_msg_id()
+        while false_data_type == data_type:
+            false_data_type = self.get_data_type_or_msg_id()
+        handler.notify.append(Module(None, false_data_type))
+        handler.notify.append(Module(None, data_type))
+        handler.notify.append(Module(None, data_type))
+        handler.notify.append(Module(None, false_data_type))
+
+        await handler._GossipHandler__send_notification(ttl, msg_id, data_type, data)
+
+        if ttl:
+            self.assertEqual(1, len(handler.waiting_for_validation))
+            self.assertEqual(peer_notification, handler.waiting_for_validation.get(msg_id)[0])
+        else:
+            self.assertEqual(0, len(handler.waiting_for_validation))
+        self.assertEqual(2, handler.api_send_queue.qsize())
+        temp = await handler.api_send_queue.get()
+        self.assertEqual(notification, temp[0])
+        self.assertTrue(handler.p2p_send_queue.empty())
+
+    def test__remove_old_waiting_stay(self):
+        handler = self.new_basic_handler()
+        handler.waiting_for_validation.update({(1).to_bytes(1, 'big'): ("1".encode(), time.time())})
+        handler.waiting_for_validation.update({(2).to_bytes(1, 'big'): ("2".encode(), time.time())})
+        handler._GossipHandler__remove_old_waiting()
+        self.assertEqual(2, len(handler.waiting_for_validation))
+
+    def test__remove_old_waiting_leave(self):
+        handler = self.new_basic_handler()
+        handler.waiting_for_validation.update({(1).to_bytes(1, 'big'): ("1".encode(), time.time() - 60)})
+        testtime = time.time()
+        handler.waiting_for_validation.update({(2).to_bytes(1, 'big'): ("2".encode(), testtime)})
+        handler._GossipHandler__remove_old_waiting()
+        self.assertEqual(1, len(handler.waiting_for_validation))
+        self.assertTrue((2).to_bytes(1, 'big') in handler.waiting_for_validation)
+        self.assertEqual(("2".encode(), testtime), handler.waiting_for_validation[(2).to_bytes(1, 'big')])
+
+    @parameterized.expand([(True,), (False,)])
+    async def test__validate(self, valid: bool):
+        handler = self.new_basic_handler()
+        msg_id = self.get_data_type_or_msg_id()
+        mock_message = self.get_data()
+        handler.waiting_for_validation.update({msg_id: (mock_message, time.time())})
+        message = (8).to_bytes(2, 'big') + (503).to_bytes(2, 'big') + msg_id
+        if valid:
+            message += (1).to_bytes(2, 'big')
+            with patch.object(GossipHandler, '_GossipHandler__remove_old_spread') as mock:
+                await handler._GossipHandler__validate(message)
+            mock.assert_called()
+            self.assertTrue(msg_id in handler.spread)
+            temp = await handler.p2p_send_queue.get()
+            self.assertEqual(mock_message, temp[0])
+        else:
+            message += (0).to_bytes(2, 'big')
+            logger = MagicMock()
+            handler.logger = logger
+            await handler._GossipHandler__validate(message)
+            logger.warning.assert_called_with(
+                "Message validation failed, message will be removed and not send: msgID %s", msg_id)
+
+    def test__remove_old_spread_stay(self):
+        handler = self.new_basic_handler()
+        msg_id_a = self.get_data_type_or_msg_id()
+        msg_id_b = self.get_data_type_or_msg_id()
+        handler.spread.update({msg_id_a: time.time()})
+        handler.spread.update({msg_id_b: time.time()})
+        handler._GossipHandler__remove_old_spread()
+        self.assertEqual(2, len(handler.spread))
+
+    def test__remove_old_spread_leave(self):
+        handler = self.new_basic_handler()
+        msg_id_a = self.get_data_type_or_msg_id()
+        msg_id_b = self.get_data_type_or_msg_id()
+        handler.spread.update({msg_id_a: time.time() - 600})
+        handler.spread.update({msg_id_b: time.time()})
+        handler._GossipHandler__remove_old_spread()
+        self.assertEqual(1, len(handler.spread))
+        self.assertTrue(msg_id_b in handler.spread)
+
+
+if __name__ == "__main__":
+    main()
